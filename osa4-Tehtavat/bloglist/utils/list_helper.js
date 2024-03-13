1867eace3708354313385981bd1aba437c2af07b
Rakuune <rakurantala@gmail.com>
2024-03-13 23:40:23 +0200

    osa 4 teh. 3-7

--- osa4-Tehtavat/bloglist/utils/list_helper.js
+++ osa4-Tehtavat/bloglist/utils/list_helper.js
@@ -1,17 +1,39 @@
-const { test, describe } = require('node:test')
-const assert = require('node:assert')
-const listHelper = require('../utils/list_helper')
-
-test('dummy returns one', () => {
-  const blogs = []
-
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
-})
 const dummy = (blogs) => {
-    // ...
+    return 1
   }
-  
+
+  const totalLikes = (blogs) => {
+    if (blogs.length === 0) {
+      return 0
+    } else if (blogs.length === 1) {
+      return blogs[0].likes
+    } else {
+      return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+    }
+  }
+
+  const favoriteBlog = (blogs) => {
+    const reducer = (last, now) => {
+      return last.likes > now.likes ? last : now
+    }
+    return blogs.length === 0 ? undefined : blogs.reduce(reducer)
+  }
+
+  const mostBlogs = (blogs) => {
+    const authors = blogs.map(blog => blog.author)
+    const author = authors.reduce((last, now) => {
+      last[now] = (last[now] || 0) + 1
+      return last
+    }, {})
+    const authorArray = Object.entries(author)
+    const reducer = (last, now) => {
+      return last > now ? last : now
+    }
+    const result = authorArray.reduce(reducer)
+    return { author: result[0], blogs: result[1] }
+  }
   module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog,
   }
